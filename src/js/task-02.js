322c022531e394d8ec44114b9c88b69b4056bf37
Fenickss <denis.suxov.1991@mail.ru>
2022-08-14 15:38:18 +0300

    +

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -8,3 +8,27 @@ const ingredients = [
 ];
 
 
+
+
+
+
+
+// const ulList = document.querySelector('#ingredients')
+
+// const ulLi = (arr) => {
+//   arr.forEach(e => {
+//     const liElem = document.createElement('li')
+//     liElem.textContent = e
+//     ulList.append(liElem)
+//   });
+// }
+
+// ulLi(ingredients)
+
+
+
+const ulEl = document.querySelector('#ingredients');
+
+const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
+
+ulEl.innerHTML = list;
--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,7 +1,7 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: 'https://images.pexels.com/photos/4046718/pexels-photo-4046718.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    alt: 'Group of Horses Running',
   },
   {
     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -9,6 +9,42 @@ const images = [
   },
   {
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    alt: 'sand drawing',
   },
 ];
+
+
+// const gallery = document.querySelector("#gallery");
+// gallery.classList.add("gallery");
+
+// const makeGalleryItemMarkup = (image) => {
+//   const { url, alt } = image;
+//   return `
+//   <li class="gallery-item">
+//    <img src= ${url} alt= ${alt} ></img>
+//   </li >`;
+// };
+
+// const makeGalleryMarkup = images.map(makeGalleryItemMarkup).join("");
+
+// console.log(makeGalleryMarkup);
+
+// gallery.insertAdjacentHTML("beforeend", makeGalleryMarkup);
+
+
+
+
+
+const ulEl = document.querySelector('#gallery');
+
+// const image = images.reduce(
+//   (string, item) => string + `<img src = "${item.url}" alt = "${item.alt}"  width = "150" height = "100" />`,
+//   ""
+// );
+// ulEl.innerHTML = image;
+images.forEach(el => {
+  ulEl.insertAdjacentHTML(
+    'afterbegin',
+    `<li><img src = "${el.url}" alt = "${el.alt}"  width = "150" height = "100" /></li>`,
+  );
+});
